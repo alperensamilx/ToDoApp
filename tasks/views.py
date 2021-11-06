56c454832d7aa08e1f4bc7643786ff01575356f4
Alperen Şamil İlmaz <71771072+alperensamilx@users.noreply.github.com>
2021-11-06 15:17:43 +0300

    second commit

--- tasks/views.py
+++ tasks/views.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 
 # Create your views here.
 
-tasks = []
+
 
 
 class NewTaskForm(forms.Form):
@@ -14,8 +14,10 @@ class NewTaskForm(forms.Form):
 
 
 def index(request):
+    if 'tasks' not in request.session:
+        request.session['tasks'] = []
     return render(request, 'tasks/index.html', {
-        'tasks': tasks
+        'tasks': request.session['tasks']
     })
 
 
@@ -24,7 +26,7 @@ def add(request):
         form = NewTaskForm(request.POST)
         if form.is_valid():
             task = form.cleaned_data['task']
-            tasks.append(task)
+            request.session['tasks'] += [task]
             return HttpResponseRedirect(reverse('index'))
         else:
             return render(request, 'tasks/add.html', {
